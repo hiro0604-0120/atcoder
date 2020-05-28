967474acf7b0ad83ad996d5150489cce132a5569
hiro0604-0120 <hiro0109play@gmail.com>
2020-05-28 20:00:13 +0900

    submission ex.10

---
+++ apg4b/Ex.10.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int A, B;
+  cin >> A >> B;
+  // ここにプログラムを追記
+  cout << "A:";
+  int i = 0;
+  while (i < A)
+  {
+    cout << "]";
+    i++;
+  }
+  cout << endl;
+
+  int j = 0;
+  cout << "B:";
+  while (j < B)
+  {
+    cout << "]";
+    j++;
+  }
+  cout << endl;
+}
