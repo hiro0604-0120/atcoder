5eb8d0c499fcb0b23e26f0be117233b309bb35c1
hiro0604-0120 <hiro0109play@gmail.com>
2020-05-28 17:32:42 +0900

    submission ex.8

---
+++ apg4b/Ex8.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int p;
+    cin >> p;
+    // パターン2
+    if (p == 2)
+    {
+        string text;
+        cin >> text;
+        cout << text << "!" << endl;
+    }
+    int price;
+    cin >> price;
+    int N;
+    cin >> N;
+    cout << price * N << endl;
+}
